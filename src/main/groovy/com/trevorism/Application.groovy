671c8a18f6a611ee0b1c1e16dddf089e7410d5c9
Trevor Brooks <trevorism@gmail.com>
2023-01-02 16:40:17 -0500

    Add logback and better API docs and responses

--- src/main/groovy/com/trevorism/Application.groovy
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -5,6 +5,8 @@ import io.micronaut.runtime.Micronaut
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @OpenAPIDefinition(
         info = @Info(
@@ -16,6 +18,7 @@ import io.swagger.v3.oas.annotations.info.Info
 )
 @CompileStatic
 class Application {
+    private static final Logger log = LoggerFactory.getLogger( Application )
 
     static void main(String[] args) {
         Micronaut.run(Application, args)
--- src/main/groovy/com/trevorism/controller/RootController.groovy
+++ src/main/groovy/com/trevorism/controller/RootController.groovy
@@ -8,40 +8,42 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @Controller("/")
 class RootController {
 
-    @Operation(summary = "say hello")
+    private static final Logger log = LoggerFactory.getLogger(RootController)
+
+
+    @Operation(summary = "Context Root of the Application")
     @ApiResponse(
-            responseCode = "200",
-            content = @Content(mediaType = "text/plain",
-                    schema = @Schema(type = "string"))
+            responseCode = "200", content = @Content(mediaType = "text/html", schema = @Schema(type = "string"))
     )
-    @Get(produces = MediaType.TEXT_PLAIN)
-    String index() {
-        return "Hello World"
+    @Get(produces = MediaType.TEXT_HTML)
+    HttpResponse<List<String>> index() {
+        log.info("Hit context root")
+        HttpResponse.ok(['<a href="/ping">/ping</a>', '<a href="/help">/help</a>', '<a href="/version">/version</a>'])
     }
 
-    @Operation(summary = "ping")
+    @Operation(summary = "Returns 'pong' on success")
     @ApiResponse(
-            responseCode = "200",
-            content = @Content(mediaType = "text/plain",
-                    schema = @Schema(type = "string"))
+            responseCode = "200", content = @Content(mediaType = "text/plain", schema = @Schema(type = "string"))
     )
     @Get(value = "/ping", produces = MediaType.TEXT_PLAIN)
     String ping() {
         return "pong"
     }
 
-    @Operation(summary = "help")
+    @Operation(summary = "This help page")
     @ApiResponse(responseCode = "302")
     @Get(value = "/help")
     HttpResponse<String> help() {
         return HttpResponse.redirect(new URI("swagger-ui/index.html"))
     }
 
-    @Operation(summary = "version")
+    @Operation(summary = "Returns the version of the API")
     @ApiResponse(
             responseCode = "200",
             content = @Content(mediaType = "text/plain",
