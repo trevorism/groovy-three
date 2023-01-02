3381e202ccce8db5bbdec52319bba6d45af9b500
Trevor Brooks <trevorism@gmail.com>
2023-01-02 00:21:32 -0500

    micronaut gcp app

---
+++ src/main/groovy/groovy/three/Application.groovy
@@ -0,0 +1,21 @@
+package groovy.three
+
+import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Groovy Three",
+                version = "1.0",
+                description = "First groovy micronaut app",
+                contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
+        )
+)
+class Application {
+
+    static void main(String[] args) {
+        Micronaut.run(Application, args)
+    }
+}
---
+++ src/main/groovy/groovy/three/controller/RootController.groovy
@@ -0,0 +1,46 @@
+package groovy.three.controller
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+
+@Controller("/")
+class RootController {
+
+    @Operation(summary = "say hello")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "text/plain",
+                    schema = @Schema(type = "string"))
+    )
+    @Get(produces = MediaType.TEXT_PLAIN)
+    String index() {
+        return "Hello World"
+    }
+
+    @Operation(summary = "ping")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "text/plain",
+                    schema = @Schema(type = "string"))
+    )
+    @Get(value = "/ping", produces = MediaType.TEXT_PLAIN)
+    String ping() {
+        return "pong"
+    }
+
+    @Operation(summary = "version")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "text/plain",
+                    schema = @Schema(type = "string"))
+    )
+    @Get(value = "/version", produces = MediaType.TEXT_PLAIN)
+    String version() {
+        return "0-1-1"
+    }
+}
